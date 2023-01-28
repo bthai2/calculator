fce4acd31abec4771323338a57523050714fc9be
Brandon Thai <brandon88thai@hotmail.com>
2023-01-27 21:22:04 -0500

    update display of calculator

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const body = document.querySelector('body');
+const operators = ['+', '-', '/', 'x'];
 
 let container = document.createElement('div');
 container.classList.add('container');
@@ -24,18 +25,29 @@ buttons.classList.add('buttons');
 let buttonArray = [];
 
 var calculation = '';
-var displayString = '';
+var calculationDisplayString = '';
 var resultString = '';
+var resultDisplayString = '';
 
 var num1, num2;
 
-function updateCalcStrings(str){
-    calculation = str;
-    displayString = calculation.slice(calculation.length-12);
-}
+var currOperator = '';
 
-function updateResult(){
-    resultString
+function updateStrings(str1, str2){
+    calculation += str1;
+    calculationDisplayString = calculation.slice(calculation.length-12);
+    resultString += str2;
+    resultDisplayString = resultString.slice(resultString.length - 12);
+
+    let display1 = document.querySelector('.calculation');
+    let display2 = document.querySelector('.result');
+
+    // if(display1.includes('/')){
+
+    // }
+
+    display1.textContent = calculationDisplayString;
+    display2.textContent = resultDisplayString;
 }
 
 function makeButton(name, onClickFunction){
@@ -57,56 +69,64 @@ function onPercentClicked(){
     console.log('%');
 }
 
-function onDivideClicked(){
-    console.log('divide');
-}
-
-function onAddClicked(){
-    console.log('Add');
-}
-
-function onSubtractClicked(){
-    console.log('Subtract');
-}
-
-function onMultiplyClicked(){
-    console.log('X');
-}
-
-function onDecimalClicked(){
-    console.log('.');
-}
-
 function onEqualClicked(){
     console.log('====');
 }
 
-function onNumberClicked(num){
-    console.log(num);
+function onOperatorClick(operator){
+
+}
+
+// function findOperator(str){
+//     let arr = operators
+//     .map(operator => str.indexOf(operator))
+//     .filter(element => element >= 0);
+//     return arr[0];
+// }
+
+function onButtonClick(str) {
+    // if string already has operator replace it
+    if(operators.includes(str) && calculation.includes(str)){
+        //switch the operator with this new one
+        calculation.replace(currOperator, str);
+        currOperator = str;
+        updateStrings('', ''); // to update the short string
+                                // if it is too long
+    } else {
+        updateStrings(str, '');
+    }
+    // if(isNaN(Number(str)) && str !== '.'){
+    //     if(hasOperator(str)){
+    //         return;
+    //     }
+    // }
+    // updateStrings(str, '');
+    console.log(calculation);
+    console.log(resultString);
 }
 
 makeButton('AC', onAcClicked);
 makeButton('C', onCClicked);
 makeButton('%', onPercentClicked);
-makeButton('/', onDivideClicked);
+makeButton('/', () => {onButtonClick('/')});
 
-makeButton('7', () => {onNumberClicked(7)});
-makeButton('8', () => {onNumberClicked(8)});
-makeButton('9', () => {onNumberClicked(9)});
-makeButton('+', onAddClicked);
+makeButton('7', () => {onButtonClick(7)});
+makeButton('8', () => {onButtonClick(8)});
+makeButton('9', () => {onButtonClick(9)});
+makeButton('+', () => {onButtonClick('+')});
 
-makeButton('4', () => {onNumberClicked(4)});
-makeButton('5', () => {onNumberClicked(5)});
-makeButton('6', () => {onNumberClicked(6)});
-makeButton('-', onSubtractClicked);
+makeButton('4', () => {onButtonClick(4)});
+makeButton('5', () => {onButtonClick(5)});
+makeButton('6', () => {onButtonClick(6)});
+makeButton('-', () => {onButtonClick('-')});
 
-makeButton('1', () => {onNumberClicked(1)});
-makeButton('2', () => {onNumberClicked(2)});
-makeButton('3', () => {onNumberClicked(3)});
-makeButton('x', onMultiplyClicked);
+makeButton('1', () => {onButtonClick(1)});
+makeButton('2', () => {onButtonClick(2)});
+makeButton('3', () => {onButtonClick(3)});
+makeButton('x', () => {onButtonClick('x')});
 
-makeButton('0', () => {onNumberClicked(0)});
-makeButton('.', onDecimalClicked);
+makeButton('0', () => {onButtonClick(0)});
+makeButton('.', () => {onButtonClick('.')});
 makeButton('=', onEqualClicked);
 
 for(let b of buttonArray){
@@ -119,6 +139,8 @@ for(let b of buttonArray){
             b.textContent = '';
             b.innerHTML = '&divide;';
             b.classList.add('division');
+        } else if(b.textContent == '.'){
+            // do nothing  
         } else {//other operators
             b.classList.add('operator');
         }
